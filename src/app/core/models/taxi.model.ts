1afe5aa5258c4c8067c83da4992ea820016b4c1d
Antoine <ant28121@hotmail.fr>
2019-03-29 16:44:29 +0100

    antoine
    debug

---
+++ src/app/core/models/taxi.model.ts
@@ -0,0 +1,12 @@
+// import {Deserializable} from "./deserializable.model";
+
+
+// export class Taxi implements Deserializable {
+//     public name: string;
+//     public img: string;
+//     public phone: string;
+//     public description: string;
+//     public appli: string;
+  
+    
+//   }
