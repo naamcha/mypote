a3719eeba068118e5eb130fb08f6b02e7b918827
Antoine <ant28121@hotmail.fr>
2019-04-04 16:21:11 +0200

    commit perso

--- src/app/taxi/taxi.page.ts
+++ src/app/taxi/taxi.page.ts
@@ -1,39 +1,9 @@
-// import { Component, OnInit } from '@angular/core';
-// import * as sitesData from '../../assets/data/sites.json';
-// import { TaxiService } from './taxi.service';
-// import { Site } from '../core/models/site.model';
-// import { ActivatedRoute } from '@angular/router';
-// import { Taxi } from '../core/models/taxi.model';
-// import { Sites } from '../core/models/sites.model.js';
-// @Component({
-//   selector: 'app-taxi',
-//   templateUrl: './taxi.page.html',
-//   styleUrls: ['./taxi.page.scss'],
-// })
-// export class TaxiPage implements OnInit {
-//   taxis: Site ;
-
-//   constructor( private TaxiService: TaxiService, private route: ActivatedRoute) { }
-
-//   ngOnInit() {
-//    const id= this.route.snapshot.params['id'];
-//    console.log(id);
-//    console.log(sitesData.sites);
-
-//   console.log(new Sites().deserialize(sitesData.sites));
-//   // créer un tableau taxi à partir de l'objet site récupérer via la fonction getsite
-//     this.taxis = this.TaxiService.getSite(+id);
-//     console.log(this.taxis);
-//   }
-
-// }
 import { Component, OnInit } from '@angular/core';
-import * as sitesData from '../../assets/data/sites.json';
+
 import { TaxiService } from './taxi.service';
-import { Site } from '../core/models/site.model';
+
 import { ActivatedRoute } from '@angular/router';
-import { Taxi } from '../core/models/taxi.model';
-import { Sites } from '../core/models/sites.model.js';
+
 @Component({
   selector: 'app-taxi',
   templateUrl: './taxi.page.html',
@@ -49,10 +19,7 @@ export class TaxiPage implements OnInit {
 
     // créer un tableau taxi à partir de l'objet site récupérer via la fonction getsite
     this.taxis = this.TaxiService.getSite(id).taxi;
-    console.log("Get site: ");
-    console.log(this.taxis);
-  //   console.log("Get taxi of site: ");
-  //   console.log(this.taxis.taxi);
+    
  }
 
 }
--- src/app/taxi/taxi.service.ts
+++ src/app/taxi/taxi.service.ts
@@ -1,27 +1,3 @@
-// import { Injectable } from '@angular/core';
-// import * as sitesData from '../../assets/data/sites.json';
-
-// import { Site } from '../core/models/site.model';
-// import { Sites } from '../core/models/sites.model';
-
-// @Injectable({
-//   providedIn: 'root'
-// })
-// export class TaxiService {
-
-//   constructor() { 
-//   }
-
-//     // récupère le json et le transforme en objet
-//    public getSites(): Sites {
-//     return new Sites().deserialize(sitesData.sites);
-//  }
-//   // recherche au sein de l'objet le site comportant l'id voulu
-//   public getSite(id): Site {
-//   return this.getSites().sites.find(site => site.id === id);
-//   }
-  
-// }
 import { Injectable } from '@angular/core';
 import * as sitesData from '../../assets/data/sites.json';
 
@@ -43,8 +19,6 @@ export class TaxiService {
   // recherche au sein de l'objet le site comportant l'id voulu
   public getSite(id): Site {
     let test = this.getSites().sites.find(site => site.id === id);
-    console.log("Taxi Service getSite : ");
-    console.log(this.getSites().sites.find(site => site.id === id));
     return test;
   }
   
