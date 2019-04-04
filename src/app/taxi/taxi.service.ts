c7d1ff5cd563343b3562a5cfc3751558371a6f79
Antoine <ant28121@hotmail.fr>
2019-04-04 10:33:39 +0200

    TAXI
    Affichage fini, 
    RESTE à faire : lien vers une appli et tester l'appel

--- src/app/taxi/taxi.service.ts
+++ src/app/taxi/taxi.service.ts
@@ -1,3 +1,27 @@
+// import { Injectable } from '@angular/core';
+// import * as sitesData from '../../assets/data/sites.json';
+
+// import { Site } from '../core/models/site.model';
+// import { Sites } from '../core/models/sites.model';
+
+// @Injectable({
+//   providedIn: 'root'
+// })
+// export class TaxiService {
+
+//   constructor() { 
+//   }
+
+//     // récupère le json et le transforme en objet
+//    public getSites(): Sites {
+//     return new Sites().deserialize(sitesData.sites);
+//  }
+//   // recherche au sein de l'objet le site comportant l'id voulu
+//   public getSite(id): Site {
+//   return this.getSites().sites.find(site => site.id === id);
+//   }
+  
+// }
 import { Injectable } from '@angular/core';
 import * as sitesData from '../../assets/data/sites.json';
 
@@ -10,7 +34,6 @@ import { Sites } from '../core/models/sites.model';
 export class TaxiService {
 
   constructor() { 
-    let sites : Sites = this.getSites();
   }
 
     // récupère le json et le transforme en objet
@@ -19,7 +42,10 @@ export class TaxiService {
  }
   // recherche au sein de l'objet le site comportant l'id voulu
   public getSite(id): Site {
-  return this.getSites().sites.find(site => site.id === id);
+    let test = this.getSites().sites.find(site => site.id === id);
+    console.log("Taxi Service getSite : ");
+    console.log(this.getSites().sites.find(site => site.id === id));
+    return test;
   }
   
 }
