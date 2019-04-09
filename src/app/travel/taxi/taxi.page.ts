30c3c0104546a8adc63fcc8562ff8b9d9d4dbe54
Antoine <ant28121@hotmail.fr>
2019-04-09 10:27:19 +0200

    test effectué, liason avec uber ok ^^

--- src/app/travel/taxi/taxi.page.ts
+++ src/app/travel/taxi/taxi.page.ts
@@ -3,6 +3,7 @@ import { Component, OnInit } from '@angular/core';
 import { TaxiService } from './taxi.service';
 
 import { ActivatedRoute } from '@angular/router';
+import { SitesService } from 'src/app/sites/sites.service';
 
 @Component({
   selector: 'app-taxi',
@@ -12,31 +13,31 @@ import { ActivatedRoute } from '@angular/router';
 export class TaxiPage implements OnInit {
   taxis: [];
 
-  constructor( private TaxiService: TaxiService, private route: ActivatedRoute) { }
+  constructor(private siteService: SitesService, private TaxiService: TaxiService, private route: ActivatedRoute) { }
 
   ngOnInit() {
-    const id= this.route.snapshot.params['id'];
+    const id = this.route.snapshot.params['id'];
 
     // créer un tableau taxi à partir de l'objet site récupérer via la fonction getsite
-    this.taxis = this.TaxiService.getSite(id).taxi;
-     
- }
-//  fonction ouvrant l'appli voulu 
- redirectTo(event){
-  const id= this.route.snapshot.params['id'];
-   const latitude=this.TaxiService.getSite(id).position.lat;
-   const longitude=this.TaxiService.getSite(id).position.lng; 
-  const address= this.TaxiService.getSite(id).formatedAddress;
-   //on récupère les variables
-   const url1= "https://m.uber.com/ul/?action=setPickup&pickup[latitude]=my_location&pickup[longitude]=my_location&pickup[nickname]=my_location&dropoff[latitude]=";
-   const url2= "&dropoff[longitude]=";
-   const url3="&dropoff[nickname]=ITCE";
-   const url4="&dropoff[formatted_address]=";
-   //const url1=this.TaxiService.getSite(id).taxi.find(idtaxi === idtaxi).url1;
+    this.taxis = this.siteService.getSite(id).taxi
+
+  }
+  //  fonction ouvrant l'appli voulu 
+  redirectTo(event) {
+    const id = this.route.snapshot.params['id'];
+    const latitude = this.siteService.getSite(id).position.lat;
+    const longitude = this.siteService.getSite(id).position.lng;
+    const address = this.siteService.getSite(id).formatedAddress;
+    //on récupère les variables
+    const url1 = "https://m.uber.com/ul/?action=setPickup&pickup[latitude]=my_location&pickup[longitude]=my_location&pickup[nickname]=my_location&dropoff[latitude]=";
+    const url2 = "&dropoff[longitude]=";
+    const url3 = "&dropoff[nickname]=ITCE";
+    const url4 = "&dropoff[formatted_address]=";
+    //const url1=this.TaxiService.getSite(id).taxi.find(idtaxi === idtaxi).url1;
     // on récupère les différentes urls depuis la page html et on crée des variables
-   const url= url1 + latitude + url2 + longitude + url3 + url4 +address;
-   console.log(url);
-   window.open(url,'_system');
- }
+    const url = url1 + latitude + url2 + longitude + url3 + url4 + address;
+    console.log(url);
+    window.open(url, '_system');
+  }
 
 }
--- src/app/travel/taxi/taxi.service.ts
+++ src/app/travel/taxi/taxi.service.ts
@@ -1,8 +1,5 @@
 import { Injectable } from '@angular/core';
-import * as sitesData from '../../../assets/data/sites.json';
 
-import { Site } from '../../core/models/site.model';
-import { Sites } from '../../core/models/sites.model';
 
 @Injectable({
   providedIn: 'root'
@@ -12,13 +9,10 @@ export class TaxiService {
   constructor() {
   }
 
-// récupère le json et le transforme en objet
-   public getSites(): Sites {
-    return new Sites().deserialize(sitesData.sites);
- }
-  // recherche au sein de l'objet le site comportant l'id voulu
-  public getSite(id): Site {
-    let test = this.getSites().sites.find(site => site.id === id);
+
+  // recherche au sein de l'objet le taxi avec l'id voulu
+  public getTaxi(site,name){
+    let test = site.taxi.find(taxi =>taxi.name === name);
     return test;
   }
 }
